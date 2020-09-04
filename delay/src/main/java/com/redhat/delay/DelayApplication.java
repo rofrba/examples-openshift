db521ddc58fa3fdff22a264154482ab53200faf1
Rodrigo Alvarez <ralvarez@redhat.com>
2020-09-04 10:01:42 -0300

    Add delay project

---
+++ delay/src/main/java/com/redhat/delay/DelayApplication.java
@@ -0,0 +1,13 @@
+package com.redhat.delay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelayApplication.class, args);
+	}
+
+}
---
+++ delay/src/test/java/com/redhat/delay/DelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.delay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
