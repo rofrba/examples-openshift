e113e704141bc47ef68f27d879d9259c692adebd
Rodrigo Alvarez <ralvarez@redhat.com>
2020-09-22 14:20:48 -0300

    Add log

--- hello-world-java/src/main/java/com/redhat/helloworld/TestController.java
+++ hello-world-java/src/main/java/com/redhat/helloworld/TestController.java
@@ -1,5 +1,7 @@
 package com.redhat.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -7,9 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+    Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping(path = "/")
     public ResponseEntity<Object> sayHello()  {  
-         
+        int i=1;
+        logger.info("Request number: "+i);
+        i++;
+    
         return ResponseEntity.status(HttpStatus.OK).body("Service UP v2!");
     }
 }
