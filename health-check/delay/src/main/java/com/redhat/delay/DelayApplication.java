bb75b462b736a685570e16f3999baa4c2d4d66f0
Rodrigo Alvarez <ralvarez@redhat.com>
2020-09-24 16:42:13 -0300

    Simulate delay in startup

--- health-check/delay/src/main/java/com/redhat/delay/DelayApplication.java
+++ health-check/delay/src/main/java/com/redhat/delay/DelayApplication.java
@@ -1,5 +1,8 @@
 package com.redhat.delay;
 
+import java.util.concurrent.TimeUnit;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DelayApplication {
 
 	public static void main(String[] args) {
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		SpringApplication.run(DelayApplication.class, args);
 	}
 
