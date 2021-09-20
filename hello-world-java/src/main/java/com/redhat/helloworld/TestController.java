2e1c62c1c9ce29c712d5f3c38b76f8c1082163b6
Your Name <you@example.com>
2021-09-20 12:51:39 -0300

    test federador

--- hello-world-java/src/main/java/com/redhat/helloworld/TestController.java
+++ hello-world-java/src/main/java/com/redhat/helloworld/TestController.java
@@ -12,11 +12,31 @@ public class TestController {
     Logger logger = LoggerFactory.getLogger(TestController.class);
     int i=1;
     @GetMapping(path = "/")
-    public ResponseEntity<Object> sayHello()  {  
-        
+    public ResponseEntity<Object> sayHello()  {
+
         logger.info("Request number: "+i);
+        insert_data("http://www.bussalud.gov.ar", "2021-06-28 15:04:38.561285", null, "http://msgc.gcba.gob.ar", null, null, "masterfile-federacion-service/fhir/Patient");
+        insert_data("hospitalitaliano.org.ar", "2021-06-28 15:04:39.127933", null, "sec.salud.muni.cordoba", "51", null, "/masterfile-federacion-service/api/personas/renaper");
+        insert_data("hospitalitaliano.org.ar", "2021-06-28 15:04:40.149939", null, "secretaria.tecnologias.gobierno.santafe", "85", null, "/masterfile-federacion-service/api/personas/renaper");
+        insert_data("hospitalitaliano.org.ar", "2021-06-28 15:04:40.648254", null, "ministerio.neuquen", "7", null, "/masterfile-federacion-service/api/personas/cobertura");
+        insert_data("hospitalitaliano.org.ar", "2021-06-28 15:04:42.560667", null, "ministerio.neuquen", "7", null, "/masterfile-federacion-service/api/personas/renaper");
+        insert_data("http://www.bussalud.gov.ar", "2021-06-28 15:04:42.567586", null, "https://app.andes.gob.ar", null, null, "masterfile-federacion-service/fhir/Patient");
+        insert_data("hospitalitaliano.org.ar", "2021-06-28 15:04:43.025366", null, "sec.salud.muni.cordoba", "51", null, "/masterfile-federacion-service/api/personas/renaper");
+        insert_data("hospitalitaliano.org.ar", "2021-06-28 15:04:44.520133", null, "ministerio.salud.sanjuan", "46", null, "/masterfile-federacion-service/api/personas/cobertura");
+        insert_data("http://www.bussalud.gov.ar", "2021-06-28 15:04:45.640256", null, "https://app.andes.gob.ar", null, null, "masterfile-federacion-service/fhir/Patient");
+        insert_data("hospitalitaliano.org.ar", "2021-06-28 15:04:48.026646", null, "ministerio.neuquen", "7", null, "/masterfile-federacion-service/api/personas/renaper");
+        insert_data("hospitalitaliano.org.ar", "2021-06-28 15:04:48.535775", null, "masterfile", "2", null, "/masterfile-federacion-service/api/personas/renaper/");
         i++;
-    
+
         return ResponseEntity.status(HttpStatus.OK).body("Service UP v3!");
+    }
+
+    public void insert_data(String iss, String created_at, String des, String sub, String user_id, String type, String path){
+        //iss       http://www.bussalud.gov.ar
+        //fecha     2021-06-28 15:04:38.561285
+        //sub       http://msgc.gcba.gob.ar
+        //user_id   null
+        //path      masterfile-federacion-service/fhir/Patient
+        logger.info("ISS: " + iss + " CREATED_AT: " + created_at + " SUB: " + sub + " USER_ID: " + user_id + " PATH: " + path);
     }
 }
